b31d9479b6c97e838a923cd76994c65736787328
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-04-17 12:52:05 +0200

    fix(core): Richardson-Lucy

--- blur_image/core.py
+++ blur_image/core.py
@@ -1,6 +1,7 @@
 import os
+import time
 from utils import load_image, save_image
-from image_processing.kernels import Kernel, apply_kernel, kernel_average, kernel_gaussian
+from image_processing.kernels import kernel_average, kernel_gaussian, apply_kernel
 from image_processing.richardson_lucy import RichardsonLucy
 
 
@@ -30,19 +31,26 @@ class ImageProcessor:
 
         # Unblurring with specified iterations
         for iterations in iterations_list:
+            start_time = time.time()
             print(f"Unblurring image with {kernel_obj} and {iterations} iterations")
             rl = RichardsonLucy(image, kernel_obj.kernel, iterations)
             unblurred_image = rl.apply()
+
+            # Calculate PSNR.
+            psnr_value = rl.calculate_psnr(image, unblurred_image)
+            print(f"PSNR: {psnr_value:.2f} dB")
+
+            duration = time.time() - start_time
             unblurred_image_path = os.path.join(
                 kernel_output_folder, f"unblurred_{iterations}-iter.png"
             )
             save_image(unblurred_image, unblurred_image_path)
+            print(f"Completed unblurring in: {duration:.2f} seconds")
 
     def process_folder(self, kernels, iterations_list):
         for filename in os.listdir(self.input_folder):
-            if filename.endswith((".png", ".jpg", ".jpeg", ".webp")):
+            if filename.endswith((".png", ".jpg", ".jpeg", ".webp", ".gif")):
                 print(f"############### Processing image: {filename} ###############")
-
                 image_path = os.path.join(self.input_folder, filename)
                 image_output_folder = os.path.join(
                     self.output_folder, os.path.splitext(filename)[0]
@@ -59,21 +67,17 @@ if __name__ == "__main__":
     input_folder = "originals"
     output_folder = "processed"
 
-    # Example kernels
     kernels = [
         kernel_average(3),
         kernel_average(5),
+        kernel_average(11),
         kernel_gaussian(3, 1.0),
         kernel_gaussian(5, 1.0),
         kernel_gaussian(3, 2.0),
         kernel_gaussian(5, 2.0),
     ]
 
-    iterations_list = [
-        5,
-        10,
-        15,
-    ]  # Number of iterations for Richardson-Lucy deconvolution
+    iterations_list = [5, 10, 15]
 
     processor = ImageProcessor(input_folder, output_folder)
     processor.process_folder(kernels, iterations_list)
