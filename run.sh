cff4496501e0ea8dbaaa391965ac81a2e138894c
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-04-21 12:30:03 +0200

    feat: Color for run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 # Display menu for user to choose which script to run
-echo "Select an option:"
-echo "1) Run core"
-echo "2) Run blind_core"
-echo "3) Run both"
+echo -e "${YELLOW}Select an option:${NC}"
+echo -e "1) ${GREEN}Run core${NC}"
+echo -e "2) ${GREEN}Run blind_core${NC}"
+echo -e "3) ${GREEN}Run both${NC}"
 read -p "Enter option: " option
 
 # Path to Python scripts
@@ -14,20 +20,20 @@ BLIND_CORE_SCRIPT="python3 blur_image/blind_core.py"
 # Execute based on user input
 case $option in
     1)
-        echo "Running core..."
+        echo -e "${YELLOW}Running core...${NC}"
         $CORE_SCRIPT
         ;;
     2)
-        echo "Running blind_core..."
+        echo -e "${YELLOW}Running blind_core...${NC}"
         $BLIND_CORE_SCRIPT
         ;;
     3)
-        echo "Running both core and blind_core..."
+        echo -e "${YELLOW}Running both core and blind_core...${NC}"
         $CORE_SCRIPT
         $BLIND_CORE_SCRIPT
         ;;
     *)
-        echo "Invalid option selected. Exiting."
+        echo -e "${RED}Invalid option selected. Exiting.${NC}"
         exit 1
         ;;
 esac
